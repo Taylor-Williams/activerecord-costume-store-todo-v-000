9e584dab1f78bf142129b6c20da57d4bc5118f2b
Taylor Williams <taylor@thewilliams.net>
2018-08-01 20:26:17 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,6 +12,9 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
     create_table :costumes do |t|
       t.string :name
+      t.float :price
+      t.float :size
+      r.string :image_url
     end
   end
 end
