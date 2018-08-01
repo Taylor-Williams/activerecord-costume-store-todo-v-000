08be62e583185969f5a101a108336fb08f5642bb
Taylor Williams <taylor@thewilliams.net>
2018-08-01 20:30:06 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+class CreateCostumeStores
+
+  def change
+    create_table :costumestore do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :number_employees
+      whether or not it's still in business
+      opening time
+      closing time
+    end
+  end
+end
