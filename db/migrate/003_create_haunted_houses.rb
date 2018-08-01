6add016751ffe30f4c3be2d8939348750c5c9598
Taylor Williams <taylor@thewilliams.net>
2018-08-01 20:35:17 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,18 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses<ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.integer :costume_inventory
+      t.integer :number_employees
+      t.boolean :in_business
+      t.float :opening_time
+      t.float :closing_time
+      t.timestamps
+    end
+  end
+end
