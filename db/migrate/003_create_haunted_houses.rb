e834e7690fe3c0f707cdf620354a7e8dcc164949
Taylor Williams <taylor@thewilliams.net>
2018-08-01 20:36:57 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,8 +7,6 @@ class CreateHauntedHouses<ActiveRecord::Migration[5.2]
       t.string :location
       t.string :theme
       t.float :price
-      t.integer :costume_inventory
-      t.integer :number_employees
       t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
